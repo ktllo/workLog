41d3d044b7195a099996223a85137a112e19f76b
LO Kam Tao Leo <leolo@leolo.org>
2015-05-23 07:08:00 +0800

    Done tree traversal

--- src/org/leolo/workHelper/TreeUtil.java
+++ src/org/leolo/workHelper/TreeUtil.java
@@ -9,11 +9,19 @@ public class TreeUtil {
 		//You should not do this
 	}
 	
-	public static void traversal(TreeNode root, PrintStream out){
-		Collection<TreeNode> set = root.getChild();
-		for(TreeNode node:set){
-			traversal(node,out);//Has issue. Fix later.
-		}
+	public static String traversalIndentation = "  ";
+
+	private static void traversal(TreeNode root, PrintStream out,int level){
+		Collection<TreeNode> nodes = root.getChildren();
+		for(int i=0;i<level;i++)
+			out.print(traversalIndentation);
+		out.println(root.name);
+		level++;
+		for(TreeNode node:nodes)
+			traversal(node,out,level);
 	}
 	
+	public static void traversal(TreeNode root, PrintStream out){
+		traversal(root,out,0);
+	}
 }
